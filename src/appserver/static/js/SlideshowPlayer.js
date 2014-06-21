ac046ffc05e0bca490bf81e6e41a3f92f02477f4
Luke Murphey <Luke@LukeMurphey.net>
2014-06-21 01:27:47 -0500

    Isolated the initialize controls function such that an error doesn't stop the entire show from stopping; the show must go on!

--- src/appserver/static/js/SlideshowPlayer.js
+++ src/appserver/static/js/SlideshowPlayer.js
@@ -416,21 +416,27 @@ SplunkSlideshow.executeWhenMatches = function(selector, fx){
  */
 SplunkSlideshow.initializeControls = function(){
 	
-	SplunkSlideshow.executeWhenMatches(".shared-appbar", function(){
-		
-		setTimeout( function() {
-			$(".shared-appbar").first().prepend('<div id="slideshow-controls" class="pull-right"></div>');
+    try{
+	
+		SplunkSlideshow.executeWhenMatches(".shared-appbar", function(){
 			
-			SplunkSlideshow.addControl('prev-button', 'SplunkSlideshow.prev');
-			SplunkSlideshow.addControl('pause-button', 'SplunkSlideshow.togglePaused');
-			SplunkSlideshow.addControl('stop-button', 'SplunkSlideshow.stop');
-			SplunkSlideshow.addControl('next-button', 'SplunkSlideshow.next');
-			
-			$('.app-name').fadeOut(500, function(){
-				$("#slideshow-controls").fadeIn(500)
-			});
-		}, 2500 );
-	});
+			setTimeout( function() {
+				$(".shared-appbar").first().prepend('<div id="slideshow-controls" class="pull-right"></div>');
+				
+				SplunkSlideshow.addControl('prev-button', 'SplunkSlideshow.prev');
+				SplunkSlideshow.addControl('pause-button', 'SplunkSlideshow.togglePaused');
+				SplunkSlideshow.addControl('stop-button', 'SplunkSlideshow.stop');
+				SplunkSlideshow.addControl('next-button', 'SplunkSlideshow.next');
+				
+				$('.app-name').fadeOut(500, function(){
+					$("#slideshow-controls").fadeIn(500)
+				});
+			}, 2500 );
+		});
+    }
+    catch(err){
+    	console.warn("Unable to initialize the show controls: " + err.message);
+    }
 }
 
 /**
