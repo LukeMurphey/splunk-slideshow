fec210ef594331512754b04c070ad0da0a4740e5
Luke Murphey <Luke@LukeMurphey.net>
2014-06-21 01:00:09 -0500

    Adding overlay controls

--- src/appserver/static/js/SlideshowPlayer.js
+++ src/appserver/static/js/SlideshowPlayer.js
@@ -33,22 +33,29 @@ SplunkSlideshow.startShow = function(){
 	
 	// Schedule the next view if the slideshow is in progress.
 	if( !SplunkSlideshow.isOnSetupPage() && SplunkSlideshow.isRunningShow() ){
+		
+		// Hide the chrome if requested
+		if( store.get('hide_chrome') ){
+			SplunkSlideshow.addStylesheet("../../../static/app/slideshow/css/HideChrome.css");
+		}
+		
+		// Load the player's CSS file
+		SplunkSlideshow.addStylesheet("../../../static/app/slideshow/css/SlideshowPlayer.css");
+		
+		$( document ).ready( function(){
+			SplunkSlideshow.initializeControls();
+		} );
+		
+		SplunkSlideshow.scheduleNextView();
 		 
-		 SplunkSlideshow.scheduleNextView();
-		 
-		 // Load the app specific resources
-		 if( store.get('load_app_resources') ){
+		// Load the app specific resources
+		if( store.get('load_app_resources') ){
 			 var current_view = SplunkSlideshow.getCurrentView();
 			 
 			 if( current_view ){
 			 	SplunkSlideshow.loadAppResources(current_view.app);
 			 }
-		 }
-		 
-		 // Hide the chrome if requested
-		 if( store.get('hide_chrome') ){
-			 SplunkSlideshow.addStylesheet("../../../static/app/slideshow/css/HideChrome.css");
-		 }
+		}
 	 }
 }
 
@@ -79,17 +86,36 @@ SplunkSlideshow.isOnSetupPage = function(){
 }
 
 /**
+ * Toggle the countdown.
+ */
+SplunkSlideshow.togglePaused = function(){
+	if(SplunkSlideshow.isRunning === true){
+		SplunkSlideshow.pause();
+	}
+	else{
+		SplunkSlideshow.unpause();
+	}
+}
+
+/**
  * Pause the countdown.
  */
 SplunkSlideshow.pause = function(){
-	 return SplunkSlideshow.isRunning = false;
+	clearInterval(SplunkSlideshow.intervalID);
+	SplunkSlideshow.isRunning = false;
+	
+	// Change the icon
+	$(".pause-button").first().addClass("play-button").removeClass("pause-button");
 }
 
 /**
  * Un-pause the countdown.
  */
 SplunkSlideshow.unpause = function(){
-	 return SplunkSlideshow.isRunning = true;
+	SplunkSlideshow.startCountdown();
+	
+	// Change the icon
+	$(".play-button").first().addClass("pause-button").removeClass("play-button");
 }
 
 /**
@@ -200,6 +226,47 @@ SplunkSlideshow.getNextView = function(){
 }
 
 /**
+ * Get the previous view in the list to show.
+ */
+SplunkSlideshow.getPreviousView = function(){
+	 
+	 // Find the next view
+	 var index_of_current_view = SplunkSlideshow.getCurrentViewIndex();
+	 var index_of_prev_view = null;
+	 
+	 if( index_of_current_view === null ){
+		 return null;
+	 }
+	 
+	 // Get the views in the show
+	 var views = store.get('views');
+	 
+	 // If we are at the first entry in the list, then rotate to the last one
+	 if( index_of_current_view === 0){
+		 index_of_prev_view = views.length - 1;
+	 }
+	 
+	 // Increment to the prior view
+	 else{
+		 index_of_prev_view = index_of_current_view + 1;
+	 }
+	 
+	 // Return the view
+	 return views[index_of_prev_view];
+}
+
+/**
+ * Start countdown.
+ */
+SplunkSlideshow.startCountdown = function(){
+	 SplunkSlideshow.intervalID = setInterval( function(){
+		 SplunkSlideshow.doCountdown();
+	 }.bind(this), SplunkSlideshow.interval );
+	
+	 SplunkSlideshow.isRunning = true;
+}
+
+/**
  * Schedule the opening of the next view.
  */
 SplunkSlideshow.scheduleNextView = function(){
@@ -231,25 +298,7 @@ SplunkSlideshow.scheduleNextView = function(){
 	 NProgress.set(0.0);
 	 
 	 // Setup the changing to the new view
-	 SplunkSlideshow.intervalID = setInterval( function(){
-		 SplunkSlideshow.doCountdown();
-	 }.bind(this), SplunkSlideshow.interval );
-}
-
-/**
- * Load the given Javascript file.
- */
-SplunkSlideshow.addJavascript = function( filename, callback ){	
-	var script = document.createElement('script');
-	script.setAttribute("type","text/javascript");
-	script.setAttribute("src", filename);
-	document.getElementsByTagName("head")[0].appendChild(script);
-	
-	if( callback ){
-		script.onload = function(){
-            callback();
-        };
-	}
+	 SplunkSlideshow.startCountdown();
 }
 
 /**
@@ -268,11 +317,11 @@ SplunkSlideshow.addStylesheet = function( filename ){
  */
 SplunkSlideshow.loadAppResources = function(app){
 	if( SplunkSlideshow.isViewSimpleXML() ){
-		SplunkSlideshow.addJavascript("../../../static/app/" + app + "/dashboard.js");
+		$.getScript("../../../static/app/" + app + "/dashboard.js");
 		SplunkSlideshow.addStylesheet("../../../static/app/" + app + "/dashboard.css");
 	}
 	else{
-		SplunkSlideshow.addJavascript("../../../static/app/" + app + "/application.js");
+		$.getScript("../../../static/app/" + app + "/application.js");
 		SplunkSlideshow.addStylesheet("../../../static/app/" + app + "/application.css");
 	}
 }
@@ -281,5 +330,89 @@ SplunkSlideshow.loadAppResources = function(app){
  * Return true of the view is defined using simple XML.
  */
 SplunkSlideshow.isViewSimpleXML = function(){
-	return document.getElementsByClassName('simplexml').length > 0;
+	return $('.simplexml').length > 0;
+}
+
+/**
+ * Stop the show and send the user to the setup page.
+ */
+SplunkSlideshow.stop = function(){
+	SplunkSlideshow.pause();
+	document.location = "slideshow_setup";
+}
+
+/**
+ * Go to the next view in the slideshow.
+ */
+SplunkSlideshow.next = function(){
+	
+	// Get the next view in the list to show
+	var next_view = SplunkSlideshow.getNextView();
+	 
+	// Stop if we couldn't find the next view
+	if( !next_view ){
+		return;
+	}
+	
+	SplunkSlideshow.pause();
+	
+	document.location = next_view.name;
+}
+
+/**
+ * Go to the previous view in the slideshow.
+ */
+SplunkSlideshow.prev = function(){
+	
+	// Get the prev view in the list to show
+	var prev_view = SplunkSlideshow.getPreviousView();
+	 
+	// Stop if we couldn't find the previous view
+	if( !prev_view ){
+		return;
+	}
+	
+	SplunkSlideshow.pause();
+	
+	document.location = prev_view.name;
+}
+
+/**
+ * Execute the provided function when the selector matches something in the DOM.
+ */
+SplunkSlideshow.executeWhenMatches = function(selector, fx){
+	if($(selector).length == 0){
+		setTimeout(function(){ SplunkSlideshow.executeWhenMatches(selector, fx); }, 100);
+	}
+	else{
+		fx();
+	}
+}
+
+/**
+ * Configures the controls.
+ */
+SplunkSlideshow.initializeControls = function(){
+	
+	SplunkSlideshow.executeWhenMatches(".shared-appbar", function(){
+		
+		setTimeout( function() {
+		$(".shared-appbar").first().prepend('<div id="slideshow-controls" class="pull-right"></div>');
+		
+		SplunkSlideshow.addControl('prev-button', 'SplunkSlideshow.prev');
+		SplunkSlideshow.addControl('pause-button', 'SplunkSlideshow.togglePaused');
+		SplunkSlideshow.addControl('stop-button', 'SplunkSlideshow.stop');
+		SplunkSlideshow.addControl('next-button', 'SplunkSlideshow.next');
+		
+		$('.app-name').fadeOut(500, function(){$("#slideshow-controls").fadeIn(500)});
+		}, 5000);
+	});
+}
+
+/**
+ * Add a control to the Slideshow controls list.
+ */
+SplunkSlideshow.addControl = function(cls, fx){
+	var html = '<div class="slideshow-control ' + cls + '"><a href="javascript:' + fx + '()"></a></li>';
+	$("#slideshow-controls").append(html);
 }
